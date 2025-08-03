7d9751ac7b765b0d05b6a453a242980d4a24bf2c
2025-CCE-E-DEV-006 <>
2025-08-03 20:18:02 +0530

    refactor : Added enum for book details

--- src/main/java/com/bnpp/kata/developmentbooks/service/CalculateBookPriceService.java
+++ src/main/java/com/bnpp/kata/developmentbooks/service/CalculateBookPriceService.java
@@ -1,11 +1,18 @@
 package com.bnpp.kata.developmentbooks.service;
 
+import com.bnpp.kata.developmentbooks.store.BooksEnum;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class CalculateBookPriceService {
 
     public double calculatePrice(int bookId, int quantity) {
-        return quantity * 50;
+
+        BooksEnum book = Arrays.stream(BooksEnum.values()).filter(bookValue-> bookValue.getId() == bookId)
+                .findFirst().orElse(null);
+
+        return book.getPrice() * quantity;
     }
 }
