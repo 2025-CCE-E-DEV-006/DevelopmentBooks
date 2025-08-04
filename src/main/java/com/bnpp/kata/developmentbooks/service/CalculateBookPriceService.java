eab6c862a5f7945ba112d84a5134cab18bc35701
2025-CCE-E-DEV-006 <>
2025-08-04 18:43:42 +0530

    feat : Three different books purchase gave a 10% discount

--- src/main/java/com/bnpp/kata/developmentbooks/service/CalculateBookPriceService.java
+++ src/main/java/com/bnpp/kata/developmentbooks/service/CalculateBookPriceService.java
@@ -20,7 +20,9 @@ public class CalculateBookPriceService {
 
         long uniqueBookCount = bookRequest.stream().map(BookRequest::getBookId).distinct().count();
 
-        if (uniqueBookCount == 2) {
+        if (uniqueBookCount == 3) {
+            discount = 0.10;
+        }else if (uniqueBookCount == 2) {
             discount = 0.05;
         }
 
